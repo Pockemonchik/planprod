c052ef0fd4fe012ab1afdbf75428cb30d5e31590
andrey <19pokemonchik99@gmail.com>
2020-09-14 18:25:56 +0300

    fix docs

--- mvd/mvd/settings.py
+++ mvd/mvd/settings.py
@@ -158,8 +158,22 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATICFILES_DIRS=[
-os.path.join(BASE_DIR,'static')
+# STATICFILES_DIRS=[
+# os.path.join(BASE_DIR,'static')
+# ]
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Конфиг в продакшн
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+)
+
+STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/planprod/static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
